09d9a8123641a56ebec97647778895041391afd9
huangjianghai174@163.com <huangjianghai174@163.com>
2021-12-11 23:31:11 +0800

    个人网站配置修改

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
   <link href="css/sousuo.css" rel="stylesheet" /> 
   <link rel="stylesheet" href="//at.alicdn.com/t/font_1230786_gdvd1b4wlz.css" /> 
   <link rel="stylesheet" href="//at.alicdn.com/t/font_1572019_yrk10qvz.css" /> 
+  <link rel="stylesheet" href="//at.alicdn.com/t/font_3006233_99rzovp6pm6.css" />
   <script src="https://cdnjs.loli.net/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
   <script src="js/xd.js"></script> 
  </head> 
@@ -33,74 +34,60 @@
   <div class="list closed"> 
    <ul>
     <li class="title"><i class="iconfont icon-changyong"></i> 常用</li> 
-    <li><a rel="nofollow" href="http://192.168.123.90:6080/" target="_blank"><i class="iconfont icon-yunpan" style="color: #2196f3;"></i>内网导航</a></li>
-    <li><a rel="nofollow" href="https://hjhai.cn" target="_blank"><i class="iconfont icon-kologom" style="color: #f03;"></i>博客</a></li>
-    <li><a rel="nofollow" href="https://wiki.hjhai.cn/dokuwiki/doku.php?id=start&do=index" target="_blank"><i class="iconfont icon-tupian" style="color: #f90;"></i>wiki</a></li>
+    <li><a rel="nofollow" href="http://192.168.123.90:6080/" target="_blank"><i class="iconfont icon-daohang-" style="color: #2196f3;"></i>内网导航</a></li>
+	<li><a rel="nofollow" href="https://www.bilibili.com/" target="_blank"><i class="iconfont icon-bili" style="color:#09e;"></i>哔哩哔哩</a></li> 
+	<li><a rel="nofollow" href="https://pan.baidu.com" target="_blank"><i class="iconfont icon-baiduyun" style="color: #148bfe;"></i>百度网盘</a></li> 
 	<!---->
     
     <li class="title"><i class="iconfont icon-youxiang"></i> 邮箱</li> 
     <li><a rel="nofollow" href="https://mail.163.com/" target="_blank"><i class="iconfont icon-wangyi" style="color: #169;"></i>网易邮箱</a></li> 
     <li><a rel="nofollow" href="https://mail.qq.com/" target="_blank"><i class="iconfont icon-QQ" style="color: #f05;"></i>QQ邮箱</a></li> 
+    <li><a rel="nofollow" href="https://mail.fiberhome.com/" target="_blank"><i class="iconfont icon-logo_fiberhome-colour" style="color: #f05;"></i></a></li>
     <!----> 
     
+
+    <li class="title"><i class="iconfont icon-yumingyuwangzhan"></i> 我的网站</li> 
+    <li><a rel="nofollow" href="https://nas.hjhai.cn/" target="_blank"><i class="iconfont icon-NASshebei" style="color:#e48;"></i>NAS</a></li> 
+    <li><a rel="nofollow" href="https://wiz.hjhai.cn/" target="_blank"><i class="iconfont icon-biji" style="color:#e02;"></i>Wiz</a></li> 
+    <li><a rel="nofollow" href="https://wiki.hjhai.cn/dokuwiki/doku.php?id=start&do=index" target="_blank"><i class="iconfont icon-wikiicon" style="color:#56f;"></i>Wiki</a></li>  
+    <li><a rel="nofollow" href="https://zfile.hjhai.cn/" target="_blank"><i class="iconfont icon-yunpan"></i>Z-file</a></li> 
+    <li><a rel="nofollow" href="https://mm.hjhai.cn/#/" target="_blank"><i class="iconfont icon-tianchongxing-" style="color:#f06;"></i>Bitwarden</a></li> 
+    <li><a rel="nofollow" href="https://hjhai.cn/" target="_blank"><i class="iconfont icon-blog"></i>BLOG</a></li> 
+    
+    
+    <!----> 
+    <li class="title"><i class="iconfont icon-kongzhitai"></i> 开发</li> 
+    <li><a rel="nofollow" href="https://www.runoob.com/" target="_blank"><i class="iconfont icon-runoob1" style="color: #f03;"></i>菜鸟</a></li> 
+    <li><a rel="nofollow" href="http://www.w3school.com.cn/" target="_blank"><i class="iconfont icon-h5" style="color:#c03;"></i>W3school</a></li> 
+    <li><a rel="nofollow" href="https://github.com/" target="_blank"><i class="iconfont icon-github"></i>Github</a></li> 
+    <li><a rel="nofollow" href="https://segmentfault.com/" target="_blank"><i class="iconfont icon-msg" style="color:#096;"></i>SF思否</a></li> 
+    <li><a rel="nofollow" href="https://msdn.itellyou.cn/" target="_blank"><i class="iconfont icon-windows" style="color: #48c;"></i>MSDN下载</a></li> 
+    
+    <!----> 
     <li class="title"><i class="iconfont icon-msg"></i> 论坛、社交</li> 
     <li><a rel="nofollow" href="https://www.52pojie.cn/" target="_blank"><i class="iconfont icon-wuaipojie" style="color: #fff;"></i>吾爱破解</a></li>
-    <li><a rel="nofollow" href="https://www.csdn.net/" target="_blank"><i class="iconfont icon-csdn" style="color: #f03;"></i>CSDN</a></li> 
+    <li><a rel="nofollow" href="https://www.csdn.net/" target="_blank"><i class="iconfont icon-CN_csdnnet" style="color: #f03;"></i>CSDN</a></li> 
     <li><a rel="nofollow" href="https://www.v2ex.com/" target="_blank"><i class="iconfont icon-vex" style="color: #2196f3;"></i>V2EX</a></li>
-    <li><a rel="nofollow" href="https://www.weibo.com" target="_blank"><i class="iconfont icon-weibo" style="color: #e6162d;"></i>微博</a></li> 
     <li><a rel="nofollow" href="https://www.zhihu.com/" target="_blank"><i class="iconfont icon-zhihu" style="color: #0084ff;"></i>知乎</a></li> 
-    <li><a rel="nofollow" href="https://www.douban.com/" target="_blank"><i class="iconfont icon-douban" style="color: #072;"></i>豆瓣</a></li> 
-    <li><a rel="nofollow" href="https://www.instagram.com" target="_blank"><i class="iconfont icon-instagram" style="color: #93009f;"></i>Instagram</a></li> 
     <li><a rel="nofollow" href="https://www.twitter.com" target="_blank"><i class="iconfont icon-twitter" style="color: #00bcff;"></i>Twitter</a></li> 
     <li><a rel="nofollow" href="https://www.facebook.com" target="_blank"><i class="iconfont icon-facebook" style="color: #4267b2;"></i>Facebook</a></li> 
     
     <!----> 
     <li class="title"><i class="iconfont icon-shipin"></i> 视频</li> 
-    <li><a rel="nofollow" href="https://www.youtube.com/" target="_blank"><i class="iconfont icon-youtube" style="color:#f03;"></i>Youtube</a></li> 
-    <li><a rel="nofollow" href="https://v.qq.com/" target="_blank"><i class="iconfont icon-tengxunshipin" style="color:#2a0;"></i>腾讯视频</a></li> 
-    <li><a rel="nofollow" href="https://www.youku.com/" target="_blank"><i class="iconfont icon-youku" style="color:#09e;"></i>优酷</a></li> 
-    <li><a rel="nofollow" href="https://www.iqiyi.com/" target="_blank"><i class="iconfont icon-aiqiyi" style="color:#1d0;"></i>爱奇艺</a></li> 
-    <li><a rel="nofollow" href="http://www.zmz2019.com/" target="_blank"><i class="iconfont icon-zimu" style="color: #067;"></i>字幕组</a></li> 
     <li><a rel="nofollow" href="http://www.acfun.cn/index.html" target="_blank"><i class="iconfont icon-acfun" style="color:#f33;"></i>ACFUN</a></li> 
     <li><a rel="nofollow" href="https://www.bilibili.com/" target="_blank"><i class="iconfont icon-bili" style="color:#09e;"></i>哔哩哔哩</a></li> 
-    <!----> 
-    <li class="title"><i class="iconfont icon-sheji"></i> 设计视觉</li> 
-    <li><a rel="nofollow" href="https://www.dribbble.com/" target="_blank"><i class="iconfont icon-dribbble" style="color:#e48;"></i>Dribbble</a></li> 
-    <li><a rel="nofollow" href="https://www.pinterest.com/" target="_blank"><i class="iconfont icon-pinterest" style="color:#e02;"></i>Pinterest</a></li> 
-    <li><a rel="nofollow" href="https://www.behance.net/" target="_blank"><i class="iconfont icon-behance" style="color:#56f;"></i>Behance</a></li>  
-    <li><a rel="nofollow" href="https://www.iconfinder.com/" target="_blank"><i class="iconfont icon-eye"></i>IconFinder</a></li> 
-    <li><a rel="nofollow" href="https://huaban.com/" target="_blank"><i class="iconfont icon-huaban" style="color:#f06;"></i>花瓣</a></li> 
-    <li><a rel="nofollow" href="https://ui.cn/" target="_blank"><i class="iconfont icon-uizhongguo"></i>UI中国</a></li> 
-    <li><a rel="nofollow" href="https://www.zcool.com.cn/" target="_blank"><i class="iconfont icon-zhanku" style="color:#f90;"></i>站酷</a></li> 
-    <li><a rel="nofollow" href="https://www.iconfont.cn/" target="_blank"><i class="iconfont icon-iconfont" style="color: #ff6019;"></i>阿里图标</a></li>
-    <li><a rel="nofollow" href="https://www.uisdc.com/" target="_blank"><i class="iconfont icon--youshe" style="color:#0aa;"></i>优设</a></li> 
+    <li><a rel="nofollow" href="https://www.youtube.com/" target="_blank"><i class="iconfont icon-youtube" style="color:#f03;"></i>Youtube</a></li> 
+    <li><a rel="nofollow" href="https://v.qq.com/" target="_blank"><i class="iconfont icon-tengxunshipin" style="color:#2a0;"></i>腾讯视频</a></li> 
+    <li><a rel="nofollow" href="https://www.iqiyi.com/" target="_blank"><i class="iconfont icon-aiqiyi" style="color:#1d0;"></i>爱奇艺</a></li> 
+    <li><a rel="nofollow" href="https://www.huya.com/" target="_blank"><i class="iconfont icon-huya" style="color:#1d0;"></i>虎牙</a></li> 
+    
     <!----> 
     <li class="title"><i class="iconfont icon-ai-tool"></i> 工具</li> 
-    <li><a rel="nofollow" href="/ip/" target="_blank"><i class="iconfont icon-IP" style="color: #02f;"></i>IP查询</a></li> 
     <li><a rel="nofollow" href="https://translate.google.cn/?hl=zh-CN" target="_blank"><i class="iconfont icon-fanyi" style="color: #02f;"></i>谷歌翻译</a></li> 
-    <li><a rel="nofollow" href="http://www.slimego.cn/" target="_blank"><i class="iconfont icon-shilaimu" style="color: #0f89c2;"></i>史莱姆</a></li> 
-    <li><a rel="nofollow" href="https://gugeji.com/" target="_blank"><i class="iconfont icon-google" style="color:#4285f4"></i>镜像</a></li> 
-    <li><a rel="nofollow" href="https://feedly.com" target="_blank"><i class="iconfont icon-feedly" style="color: #11dd00;"></i>Feedly</a></li> 
-    <li><a rel="nofollow" href="https://pan.baidu.com" target="_blank"><i class="iconfont icon-baiduyun" style="color: #148bfe;"></i>百度网盘</a></li> 
-    <li><a rel="nofollow" href="https://www.baiduwp.com" target="_blank"><i class="iconfont icon-cloud-download" style="color: #148bfe;"></i>网盘解析</a></li> 
-    <li><a rel="nofollow" href="https://www.mdeditor.com/" target="_blank"><i class="iconfont icon-md" style="color:#09f;"></i>MD编辑器</a></li> 
     <li><a rel="nofollow" href="https://miku.tools/" target="_blank"><i class="iconfont icon-ai-tool"></i>Miku工具</a></li> 
-    <li><a rel="nofollow" href="http://cubic-bezier.com" target="_blank"><i class="iconfont icon-quxian" style="color: #f08;"></i>贝赛尔曲线</a></li> 
-    <li><a rel="nofollow" href="/base64/" target="_blank"><i class="iconfont icon-base64" style="color:#1d0;"></i>Base64</a></li> 
-    <li><a rel="nofollow" href="https://javascriptobfuscator.com/Javascript-Obfuscator.aspx" target="_blank"><i class="iconfont icon-jshunxiao" style="color: #48c;"></i>JS混淆器</a></li> 
     <li><a rel="nofollow" href="https://ping.pe" target="_blank"><i class="iconfont icon-wangluo1" style="color:#1d0;"></i>Ping.pe</a></li> 
     <li><a rel="nofollow" href="https://ping.chinaz.com/" target="_blank"><i class="iconfont icon-pingup" style="color:#2361ad;"></i>站长Ping</a></li> 
-    <!----> 
-    <li class="title"><i class="iconfont icon-kongzhitai"></i> 开发</li> 
-    <li><a rel="nofollow" href="http://www.w3school.com.cn/" target="_blank"><i class="iconfont icon-h5" style="color:#c03;"></i>W3school</a></li> 
-    <li><a rel="nofollow" href="https://github.com/" target="_blank"><i class="iconfont icon-github"></i>Github</a></li> 
-    <li><a rel="nofollow" href="https://codepen.io/" target="_blank"><i class="iconfont icon-codepen"></i>Codepen</a></li> 
-    <li><a rel="nofollow" href="https://segmentfault.com/" target="_blank"><i class="iconfont icon-msg" style="color:#096;"></i>SF思否</a></li> 
-    <li><a rel="nofollow" href="https://cdnjs.com/" target="_blank"><i class="iconfont icon-cdnjs" style="color: #e52;"></i>CdnJs</a></li> 
-    <li><a rel="nofollow" href="https://fontawesome.com/icons?d=gallery&amp;m=free" target="_blank"><i class="iconfont icon-font-awesome" style="color: #4af;"></i>Font A.</a></li> 
-    <li><a rel="nofollow" href="https://msdn.itellyou.cn/" target="_blank"><i class="iconfont icon-windows" style="color: #48c;"></i>MSDN下载</a></li> 
-    <li><a rel="nofollow" href="https://dash.cloudflare.com/" target="_blank"><i class="iconfont icon-cloudflare" style="color: #f37f20;"></i>CFcdn</a></li> 
-    <li><a rel="nofollow" href="https://www.swiper.com.cn/" target="_blank"><i class="iconfont icon-S" style="color:#065fe3;"></i>Swiper</a></li> 
+
    </ul> 
   </div> 
   <div id="search" class="s-search">
